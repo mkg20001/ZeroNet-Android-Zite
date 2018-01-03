4e385878c29ae03c4499705de6a2e5fc8b71406a
mkg20001 <mkg20001@gmail.com>
2018-01-03 16:58:43 +0100

    Initial Commit

---
+++ js/page.js
@@ -0,0 +1,31 @@
+class Page extends ZeroFrame {
+  setSiteInfo (site_info) {
+
+  }
+
+  onOpenWebsocket () {
+    this.cmd('serverInfo', [], (info) => {
+      this.cmd('fileGet', ['metadata.json'], data => {
+        const meta = JSON.parse(data)
+        meta.full = 'v' + meta.ver + ' (rev ' + meta.rev + ')'
+        $('.curversion').text(meta.full)
+        if (meta.rev === info.rev) {
+          $('#isuptodate').addClass('ch-ok').text('Up-to-date!')
+        } else if (meta.ver === info.version) {
+          $('#isuptodate').addClass('ch-ok').text('Some revisions behind!')
+        } else {
+          $('#isuptodate').addClass('ch-nok').text('Outdated! Please upgrade!')
+        }
+      })
+    })
+  }
+
+  onRequest (cmd, message) {
+    if (cmd == 'setSiteInfo') {
+      this.setSiteInfo(message.params)
+    } else {
+      this.log('Unknown incoming message:', cmd)
+    }
+  }
+}
+page = new Page()
